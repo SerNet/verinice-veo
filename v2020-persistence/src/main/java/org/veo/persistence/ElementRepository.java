0ffe3392f7bb7ee1f89b959c68360dcb02bc1c90
Urs Zeidler <uzeidler@sernet.de>
2017-06-29 10:14:49 +0200

    add simple method to get the root uuids
    A simple methods to get the root object ids.

--- v2020-persistence/src/main/java/org/veo/persistence/ElementRepository.java
+++ v2020-persistence/src/main/java/org/veo/persistence/ElementRepository.java
@@ -20,15 +20,16 @@ import org.veo.model.Element;
  */
 public interface ElementRepository extends CrudRepository<Element, String> {
 
+    @Query(value = "select uuid from element e where e.parent_uuid is null", nativeQuery = true)
+    public List<String> allRootElements();
+
     @Query("SELECT e FROM Element e join fetch e.children where e.uuid = :uuid")
     public Element findOneWithChildren(@Param("uuid") String uuid);
 
-    @Query("SELECT e FROM Element e "
-            + "left join fetch e.linksOutgoing "
-            + "left join fetch e.linksIncoming "
-            + "where e.uuid = :uuid")
-    @EntityGraph(value = "linksWithProperties" , type=EntityGraphType.LOAD)
+    @Query("SELECT e FROM Element e " + "left join fetch e.linksOutgoing "
+            + "left join fetch e.linksIncoming " + "where e.uuid = :uuid")
+    @EntityGraph(value = "linksWithProperties", type = EntityGraphType.LOAD)
     public Element findOneWithLinks(@Param("uuid") String uuid);
-    
+
     public List<Element> findByTypeId(@Param("typeId") String typeId);
 }
