c13e8dd67c84ba5c679ff0b9b9fd147470fb2d88
Daniel Murygin <dm@sernet.de>
2018-02-20 14:12:26 +0100

    Fix imports
    VEO-11

--- v2020-model-schema/src/main/java/org/veo/schema/rest/ModelSchemaRestService.java
+++ v2020-model-schema/src/main/java/org/veo/schema/rest/ModelSchemaRestService.java
@@ -21,6 +21,7 @@
 package org.veo.schema.rest;
 
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
