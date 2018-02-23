7e81da54a6d15a7cd80594c16a186c1dd50c0502
Jochen Kemnade <jk@sernet.de>
2018-02-23 11:38:10 +0100

    add tests for XmlIO

---
+++ v2020-service/src/test/groovy/org/veo/util/io/XmlIOSpec.groovy
@@ -0,0 +1,52 @@
+package org.veo.util.io
+
+import java.nio.file.Files
+
+import javax.xml.bind.JAXB
+import javax.xml.bind.JAXBContext
+
+import groovy.xml.MarkupBuilder
+import spock.lang.Specification
+
+class XmlIOSpec extends Specification {
+
+    def "read a Person from an XML file"(){
+        setup:
+        File file = Files.createTempFile('xmliospec', '.xml').toFile();
+        file.text = '''<person><age>42</age><name>John Doe</name></person>'''
+
+        when:
+        def person = XmlIO.read(file.absolutePath, Person)
+
+        then:
+        person.name == 'John Doe'
+        person.age == 42
+
+        cleanup:
+        file?.delete()
+    }
+
+    def "serialize a Person to an XML string"(){
+        setup:
+        def person = new Person()
+        person.name = 'Guybrush Threepwood'
+        person.age = 23
+        File file = Files.createTempFile('xmliospec', '.xml').toFile();
+        
+        when:
+        JAXBContext jaxbContext = JAXBContext.newInstance(Person)
+        XmlIO.write(jaxbContext, null, file.absolutePath, person)
+        
+        then:
+        file.text ==
+                '''\
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<person>
+    <age>23</age>
+    <name>Guybrush Threepwood</name>
+</person>
+'''
+        cleanup:
+        file?.delete()
+    }
+}
