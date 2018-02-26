b8f5ed44c286f7afe1502e6094498c64f3362bc9
Jochen Kemnade <jk@sernet.de>
2018-02-26 13:08:21 +0100

    restore alternate formatting approach, enable test

--- v2020-model/src/main/java/org/veo/util/time/TimeFormatter.java
+++ v2020-model/src/main/java/org/veo/util/time/TimeFormatter.java
@@ -26,50 +26,174 @@ package org.veo.util.time;
  */
 public final class TimeFormatter {
 
+    private static final int MILLIS_PER_SECOND = 1000;
+
+    private static final int MINUTES_PER_HOUR = 60;
+
+    private static final int HOURS_PER_DAY = 24;
+
+    private static final int SECONDS_PER_MINUTE = 60;
+
+    private static final int MILLIS_PER_MINUTE = MILLIS_PER_SECOND * SECONDS_PER_MINUTE;
+
+    private static final int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR;
+
+    private static final int MILLIS_PER_HOUR = MILLIS_PER_SECOND * SECONDS_PER_HOUR;
+
+    private static final int SECONDS_PER_DAY = SECONDS_PER_HOUR * HOURS_PER_DAY;
+
+    private static final long MILLIS_PER_DAY = SECONDS_PER_DAY * 1000L;
+
     private TimeFormatter() {
         super();
         // do not instantiate this class
     }
 
-    public static String getHumanRedableTime(long ms) {
-        long miliseconds = ms % 1000;
-        long x = (ms / 1000l);
-        long seconds = x % 60;
-        x /= 60;
-        long minutes = x % 60;
-        x /= 60;
-        long hours = x % 24;
-        x /= 24;
-        long days = x;
+    public static String getHumanRedableTime(final long ms) {
+        Interval interval = new Interval(ms);
+        boolean outputDays;
+        boolean outputHours;
+        boolean outputMinutes;
+        boolean outputSeconds;
+        boolean outputMilliseconds;
+
+        if (interval.days > 0) {
+            outputDays = true;
+            outputMinutes = false;
+            outputSeconds = false;
+            outputMilliseconds = false;
+            interval = interval.roundToHours();
+            outputHours = interval.hours > 0;
+        } else if (interval.hours > 0) {
+            outputDays = false;
+            outputHours = true;
+            outputSeconds = false;
+            outputMilliseconds = false;
+            interval = interval.roundToMinutes();
+            outputMinutes = interval.minutes > 0;
+        } else if (interval.minutes > 0) {
+            outputDays = false;
+            outputHours = false;
+            outputMinutes = true;
+            outputMilliseconds = false;
+            interval = interval.roundToSeconds();
+            outputSeconds = interval.seconds > 0;
+        } else {
+            outputDays = false;
+            outputHours = false;
+            outputMinutes = false;
+            outputSeconds = interval.seconds > 0;
+            outputMilliseconds = !outputSeconds;
+            if (!outputMilliseconds) {
+                interval = interval.roundToSeconds();
+            }
+        }
+
+        return formatInterval(interval, outputDays, outputHours, outputMinutes, outputSeconds,
+                outputMilliseconds);
+    }
+
+    private static String formatInterval(Interval interval, boolean outputDays, boolean outputHours,
+            boolean outputMinutes, boolean outputSeconds, boolean outputMilliseconds) {
         StringBuilder sb = new StringBuilder();
-        if (days > 0) {
-            sb.append(days).append(" d");
+
+        if (outputDays) {
+            sb.append(interval.days).append(" d");
         }
-        if (hours > 0) {
+        if (outputHours) {
             if (sb.length() > 0) {
                 sb.append(", ");
             }
-            sb.append(hours).append(" h");
+            sb.append(interval.hours).append(" h");
         }
-        if (minutes > 0 && days < 1) {
+        if (outputMinutes) {
             if (sb.length() > 0) {
                 sb.append(", ");
             }
-            sb.append(minutes).append(" m");
+            sb.append(interval.minutes).append(" m");
         }
-        if (seconds > 0 && hours < 1) {
+        if (outputSeconds) {
             if (sb.length() > 0) {
                 sb.append(", ");
             }
-            sb.append(seconds).append(" s");
+            sb.append(interval.seconds).append(" s");
         }
-        if (miliseconds > 0 && seconds < 1) {
+        if (outputMilliseconds) {
             if (sb.length() > 0) {
                 sb.append(", ");
             }
-            sb.append(miliseconds).append(" ms");
+            sb.append(interval.milliseconds).append(" ms");
         }
         return sb.toString();
+    }
+
+    private static final class Interval {
+        final int milliseconds;
+        final int seconds;
+        final int minutes;
+        final int hours;
+        final int days;
+
+        Interval(long milliseconds) {
+            this.milliseconds = (int) milliseconds % 1000;
+            seconds = (int) ((milliseconds / MILLIS_PER_SECOND) % SECONDS_PER_MINUTE);
+            minutes = (int) ((milliseconds / MILLIS_PER_MINUTE) % MINUTES_PER_HOUR);
+            hours = (int) ((milliseconds / MILLIS_PER_HOUR) % HOURS_PER_DAY);
+            days = (int) (milliseconds / MILLIS_PER_DAY);
+        }
+
+        Interval(int days, int hours, int minutes, int seconds, int milliseconds) {
+            this.days = days;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.milliseconds = milliseconds;
+        }
+
+        Interval roundToSeconds() {
+            int newSeconds = seconds;
+            int newMinutes = minutes;
+            if (milliseconds >= 500) {
+                newSeconds += 1;
+
+                if (newSeconds == SECONDS_PER_MINUTE) {
+                    newMinutes = newMinutes + 1;
+                    newSeconds = 0;
+                }
+            }
+
+            return new Interval(days, hours, newMinutes, newSeconds, 0);
+        }
+
+        public Interval roundToMinutes() {
+            int newMinutes = minutes;
+            int newHours = hours;
+            if (seconds >= 30) {
+                newMinutes += 1;
+
+                if (newMinutes == MINUTES_PER_HOUR) {
+                    newHours = newHours + 1;
+                    newMinutes = 0;
+                }
+            }
+
+            return new Interval(days, newHours, newMinutes, 0, 0);
+        }
+
+        public Interval roundToHours() {
+            int newDays = days;
+            int newHours = hours;
+            if (minutes >= 30) {
+                newHours += 1;
+
+                if (newHours == HOURS_PER_DAY) {
+                    newDays = newDays + 1;
+                    newHours = 0;
+                }
+            }
+
+            return new Interval(newDays, newHours, 0, 0, 0);
+        }
     }
 
 }
--- v2020-model/src/test/groovy/org/veo/util/time/TimeFormatterSpec.groovy
+++ v2020-model/src/test/groovy/org/veo/util/time/TimeFormatterSpec.groovy
@@ -20,11 +20,10 @@ class TimeFormatterSpec extends Specification {
         Duration.ofHours(3)                              | '3 h'
         Duration.ofHours(2).plusMinutes(30)              | '2 h, 30 m'
         Duration.ofDays(1).plusHours(2).plusMinutes(3)   | '1 d, 2 h'
-        Duration.ofDays(1).plusHours(2).plusMinutes(29)   | '1 d, 2 h, 29m'
+        Duration.ofDays(1).plusHours(2).plusMinutes(29)  | '1 d, 2 h, 29m'
         Duration.ofDays(1).plusHours(2)                  | '1 d, 2 h'
         Duration.ofDays(7)                               | '7 d'
-        // this does not work
-        // Duration.ofMinutes(2).minusMillis(1)             | '2 m'
+        Duration.ofMinutes(2).minusMillis(1)             | '2 m'
         Duration.ofMinutes(2).plusSeconds(30)            | '2 m, 30 s'
         
         milliSeconds = duration.toMillis()
