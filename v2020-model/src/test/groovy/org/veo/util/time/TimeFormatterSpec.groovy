b35d63f8cc6d42c77b6906097389a67b5f9b54bd
Daniel Murygin <dm@sernet.de>
2018-02-26 12:18:59 +0100

    Add to more test cases to time formatter test

--- v2020-model/src/test/groovy/org/veo/util/time/TimeFormatterSpec.groovy
+++ v2020-model/src/test/groovy/org/veo/util/time/TimeFormatterSpec.groovy
@@ -16,9 +16,11 @@ class TimeFormatterSpec extends Specification {
         duration                                         | humanReadable
         Duration.ofMillis(1)                             | '1 ms'
         Duration.ofMillis(1024)                          | '1 s'
+        Duration.ofMillis(1624)                          | '2 s'
         Duration.ofHours(3)                              | '3 h'
         Duration.ofHours(2).plusMinutes(30)              | '2 h, 30 m'
         Duration.ofDays(1).plusHours(2).plusMinutes(3)   | '1 d, 2 h'
+        Duration.ofDays(1).plusHours(2).plusMinutes(29)   | '1 d, 2 h, 29m'
         Duration.ofDays(1).plusHours(2)                  | '1 d, 2 h'
         Duration.ofDays(7)                               | '7 d'
         // this does not work
