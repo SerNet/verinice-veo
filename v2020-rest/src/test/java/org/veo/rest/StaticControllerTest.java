bd3042bf37f5548534a230f02f6e6967dde7a552
Alexander Ben Nasrallah <an@sernet.de>
2018-04-30 15:20:03 +0200

    Add test to cover StaticController
    VEO-17

---
+++ v2020-rest/src/test/java/org/veo/rest/StaticControllerTest.java
@@ -0,0 +1,34 @@
+package org.veo.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class StaticControllerTest {
+    @Autowired
+    private StaticController staticController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(staticController).build();
+    }
+
+    @Test
+    public void getSchema() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/schemas/test.json").accept("application/schema+json"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/schema+json; charset=utf-8"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("Test JSON with non-ascii chars: äüöÄÜÖ€"));
+    }
+}
