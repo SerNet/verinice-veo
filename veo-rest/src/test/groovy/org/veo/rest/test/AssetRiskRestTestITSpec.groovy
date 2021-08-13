39de66ffcc9be7985ff47956d5b37fb2aa290e22
Jonas Jordan <jj@sernet.de>
2021-08-13 13:34:04 +0200

    migrate asset risk & process risk postman tests
    VEO-447

---
+++ veo-rest/src/test/groovy/org/veo/rest/test/AssetRiskRestTestITSpec.groovy
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * verinice.veo
+ * Copyright (C) 2021  Jonas Jordan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.veo.rest.test
+
+class AssetRiskRestTestITSpec extends VeoRestTest{
+
+    String unitId
+    String domainId
+
+    def setup() {
+        unitId = post("/units", [name: "asset risk rest test unit"]).body.resourceId
+        domainId = get("/domains").body[0].id
+    }
+
+    def "create and update an asset risk"() {
+        given: "a composite asset and a scenario"
+        def subAssetId = post("/assets", [
+            name: "sub asset",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+        def assetId = post("/assets", [
+            domains: [
+                [targetUri: "/domains/$domainId"]
+            ],
+            parts: [
+                // The part is not relevant for the risk, it just spices things up.
+                [targetUri: "/assets/$subAssetId"]
+            ],
+            name: "risk test asset",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+        def scenarioId = post("/scenarios", [
+            name: "asset risk test scenario",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+
+        when: "creating the risk"
+        post("/assets/$assetId/risks", [
+            domains: [
+                [targetUri: "/domains/$domainId"]
+            ],
+            scenario: [targetUri: "/scenarios/$scenarioId"]
+        ])
+
+        then: "it can be retrieved"
+        def retrievedRiskResponse = get("/assets/$assetId/risks/$scenarioId")
+        def risk = retrievedRiskResponse.body
+        risk.scenario.targetUri ==~ /.*\/scenarios\/$scenarioId/
+
+        when: "assigning a risk owner"
+        def ownerPersonId = post("/persons", [
+            name: "asset risk owner",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+        risk.riskOwner = [targetUri: "/persons/$ownerPersonId"]
+        put("/assets/$assetId/risks/$scenarioId", risk, retrievedRiskResponse.headers["ETag"].toString())
+
+        then: "the risk has an owner"
+        get("/assets/$assetId/risks/$scenarioId").body.riskOwner.targetUri ==~ /.*\/persons\/$ownerPersonId/
+    }
+}
---
+++ veo-rest/src/test/groovy/org/veo/rest/test/ProcessRiskRestTestITSpec.groovy
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * verinice.veo
+ * Copyright (C) 2021  Jonas Jordan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.veo.rest.test
+
+class ProcessRiskRestTestITSpec extends VeoRestTest{
+
+    String unitId
+    String domainId
+
+    def setup() {
+        unitId = post("/units", [name: "process risk rest test unit"]).body.resourceId
+        domainId = get("/domains").body[0].id
+    }
+
+    def "create and update a process risk"() {
+        given: "a composite process and a scenario"
+        def subProcessId = post("/processes", [
+            name: "sub process",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+        def processId = post("/processes", [
+            domains: [
+                [targetUri: "/domains/$domainId"]
+            ],
+            parts: [
+                // The part is not relevant for the risk, it just spices things up.
+                [targetUri: "/processes/$subProcessId"]
+            ],
+            name: "risk test process",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+        def scenarioId = post("/scenarios", [
+            name: "process risk test scenario",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+
+        when: "creating the risk"
+        post("/processes/$processId/risks", [
+            domains: [
+                [targetUri: "/domains/$domainId"]
+            ],
+            scenario: [targetUri: "/scenarios/$scenarioId"]
+        ])
+
+        then: "it can be retrieved"
+        def retrievedRiskResponse = get("/processes/$processId/risks/$scenarioId")
+        def risk = retrievedRiskResponse.body
+        risk.scenario.targetUri ==~ /.*\/scenarios\/$scenarioId/
+
+        when: "assigning a risk owner"
+        def ownerPersonId = post("/persons", [
+            name: "process risk owner",
+            owner: [targetUri: "/units/$unitId"]
+        ]).body.resourceId
+        risk.riskOwner = [targetUri: "/persons/$ownerPersonId"]
+        put("/processes/$processId/risks/$scenarioId", risk, retrievedRiskResponse.headers["ETag"].toString())
+
+        then: "the risk has an owner"
+        get("/processes/$processId/risks/$scenarioId").body.riskOwner.targetUri ==~ /.*\/persons\/$ownerPersonId/
+    }
+}
