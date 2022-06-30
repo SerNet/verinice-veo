0050964d2ae8d9f0a0a16f4c2ef74174613e0d56
Jochen Kemnade <jk@sernet.de>
2022-06-30 11:28:07 +0200

    add REST test for the type definitions controller
    VEO-963

---
+++ veo-rest/src/test/groovy/org/veo/rest/test/TypeDefinitionsRestTest.groovy
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * verinice.veo
+ * Copyright (C) 2022  Jochen Kemnade
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.veo.rest.test
+
+import org.veo.core.entity.EntityType
+
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class TypeDefinitionsRestTest extends VeoRestTest {
+
+    def "Fetch type definitions"() {
+        when:
+        def response = get("/types")
+        then:
+        response.body.size() == EntityType.ELEMENT_TYPES.size()
+    }
+}
