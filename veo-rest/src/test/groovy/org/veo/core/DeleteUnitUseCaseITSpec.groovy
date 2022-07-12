013b5f89363c8b4ec21b0597c13a52ba168c7746
Jochen Kemnade <jk@sernet.de>
2022-07-12 16:35:51 +0200

    add an integration test for the delete unit usecase
    VEO-1428

---
+++ veo-rest/src/test/groovy/org/veo/core/DeleteUnitUseCaseITSpec.groovy
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * verinice.veo
+ * Copyright (C) 2022  Jochen Kemnade.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.veo.core
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.test.context.support.WithUserDetails
+
+import org.veo.core.entity.Client
+import org.veo.core.entity.Unit
+import org.veo.core.usecase.unit.CreateDemoUnitUseCase
+import org.veo.core.usecase.unit.DeleteUnitUseCase
+import org.veo.core.usecase.unit.DeleteUnitUseCase.InputData
+import org.veo.persistence.access.ClientRepositoryImpl
+
+import net.ttddyy.dsproxy.QueryCountHolder
+
+@WithUserDetails("user@domain.example")
+class DeleteUnitUseCaseITSpec extends AbstractPerformaceITSpec {
+
+    @Autowired
+    private ClientRepositoryImpl clientRepository
+
+    @Autowired
+    private CreateDemoUnitUseCase createDemoUnitUseCase
+
+    @Autowired
+    private DeleteUnitUseCase deleteUnitUseCase
+
+    def "delete a demo unit"() {
+        given: 'a client with a demo unit'
+        def client = createTestClient()
+        createTestDomain(client, DSGVO_DOMAINTEMPLATE_UUID)
+        def demoUnit = createDemoUnit(client)
+        QueryCountHolder.clear()
+
+        when: 'executing the DeleteUnitUseCase'
+        def unit = runUseCase(demoUnit)
+        def queryCounts = QueryCountHolder.grandTotal
+
+        then: 'query statistics show sensible data'
+        verifyAll {
+            queryCounts.select == 114
+            queryCounts.insert == 5
+            queryCounts.update == 2
+            queryCounts.delete == 39
+            queryCounts.time < 1000
+        }
+    }
+
+    def runUseCase(Unit unit) {
+        executeInTransaction {
+            deleteUnitUseCase.execute(new InputData(unit.id, unit.client))
+        }
+    }
+
+    Client createClient() {
+        executeInTransaction {
+            def client = newClient()
+            defaultDomainCreator.addDefaultDomains(client)
+            return clientRepository.save(client)
+        }
+    }
+
+    Unit createDemoUnit(Client client) {
+        def unit = txTemplate.execute {
+            createDemoUnitUseCase.execute(new CreateDemoUnitUseCase.InputData(client.id)).unit
+        }
+        unit
+    }
+}
