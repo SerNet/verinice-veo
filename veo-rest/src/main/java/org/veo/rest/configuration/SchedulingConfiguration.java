e52d4c7127312ce89b464a65e4230550cd067388
Jochen Kemnade <jk@sernet.de>
2022-08-08 10:15:07 +0200

    replace default single-thread executor to allow for parallel job executions
    VEO-1116

--- veo-rest/src/main/java/org/veo/rest/configuration/SchedulingConfiguration.java
+++ veo-rest/src/main/java/org/veo/rest/configuration/SchedulingConfiguration.java
@@ -17,11 +17,28 @@
  ******************************************************************************/
 package org.veo.rest.configuration;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @ConditionalOnProperty(value = "veo.scheduler.active", havingValue = "true")
 @Configuration
 @EnableScheduling
-public class SchedulingConfiguration {}
+public class SchedulingConfiguration implements SchedulingConfigurer {
+
+  @Override
+  public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+    taskRegistrar.setScheduler(taskExecutor());
+  }
+
+  @Bean(destroyMethod = "shutdown")
+  public Executor taskExecutor() {
+    return Executors.newScheduledThreadPool(2);
+  }
+}
