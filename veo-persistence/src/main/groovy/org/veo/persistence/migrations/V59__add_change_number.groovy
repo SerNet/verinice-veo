2caac687bb7e93249889d3af5339cce75aa32579
Alexander Koderman <ak@sernet.de>
2023-06-08 11:22:30 +0200

    Fix changeNumber migration
    VEO-2078

--- veo-persistence/src/main/groovy/org/veo/persistence/migrations/V59__add_change_number.groovy
+++ veo-persistence/src/main/groovy/org/veo/persistence/migrations/V59__add_change_number.groovy
@@ -28,28 +28,47 @@ class V59__add_change_number extends BaseJavaMigration {
         new Sql(context.connection).with {
             execute("""
                 alter table abstractriskdata
-                   add column change_number int8 not null;
-
+                   add column change_number int8;
                 alter table catalog
-                   add column change_number int8 not null;
-
+                   add column change_number int8;
                 alter table catalogitem
-                   add column change_number int8 not null;
-
+                   add column change_number int8;
                 alter table client
-                   add column change_number int8 not null;
-
+                   add column change_number int8;
                 alter table domain
-                   add column change_number int8 not null;
-
+                   add column change_number int8;
                 alter table domaintemplate
-                   add column change_number int8 not null;
-
+                   add column change_number int8;
                 alter table element
-                   add column change_number int8 not null;
-
+                   add column change_number int8;
                 alter table unit
-                   add column change_number int8 not null;
+                   add column change_number int8;
+
+                UPDATE abstractriskdata SET change_number = version;
+                UPDATE catalog SET change_number = version;
+                UPDATE catalogitem SET change_number = version;
+                UPDATE client SET change_number = version;
+                UPDATE domain SET change_number = version;
+                UPDATE domaintemplate SET change_number = version;
+                UPDATE element SET change_number = version;
+                UPDATE unit SET change_number = version;
+
+                ALTER TABLE abstractriskdata
+                    ALTER COLUMN change_number SET NOT NULL;
+                ALTER TABLE catalog
+                    ALTER COLUMN change_number SET NOT NULL;
+                ALTER TABLE catalogitem
+                    ALTER COLUMN change_number SET NOT NULL;
+                ALTER TABLE client
+                    ALTER COLUMN change_number SET NOT NULL;
+                ALTER TABLE domain
+                    ALTER COLUMN change_number SET NOT NULL;
+                ALTER TABLE domaintemplate
+                    ALTER COLUMN change_number SET NOT NULL;
+                ALTER TABLE element
+                    ALTER COLUMN change_number SET NOT NULL;
+                ALTER TABLE unit
+                    ALTER COLUMN change_number SET NOT NULL;
             """)
         }
     }
