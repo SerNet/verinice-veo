a0399b201b8555aeed65b9f0c84d564a0020e522
Urs Zeidler <uz@sernet.de>
2024-02-12 09:48:47 +0100

    add missing interfaces etc to use Flyweight
    verinice-veo#2588

--- veo-core-entity/src/main/java/org/veo/core/entity/FlyweightLink.java
+++ veo-core-entity/src/main/java/org/veo/core/entity/FlyweightLink.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * verinice.veo
- * Copyright (C) 2024  Alexander Koderman
+ * Copyright (C) 2024  Urs Zeidler
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -15,10 +15,12 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package org.veo.persistence.access;
+package org.veo.core.entity;
 
-import java.util.Set;
+public interface FlyweightLink {
+  String getSourceId();
 
-import org.veo.persistence.entity.FlyweightLink;
+  String getTargetId();
 
-public record FlyweightElement(String sourceId, String dType, Set<FlyweightLink> links) {}
+  String getType();
+}
---
+++ veo-persistence/src/main/java/org/veo/persistence/access/jpa/FlyweightLinkDataRepostory.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * verinice.veo
+ * Copyright (C) 2024  Alexander Koderman.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.veo.persistence.access.jpa;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import org.veo.core.entity.FlyweightElement;
+import org.veo.core.entity.FlyweightLink;
+import org.veo.persistence.entity.jpa.ElementData;
+import org.veo.persistence.entity.jpa.FlyweightElementData;
+
+public interface FlyweightLinkDataRepostory extends JpaRepository<ElementData, String> {
+
+  @Query(
+      value =
+          "SELECT source_id AS sourceId, target_id AS targetId, type "
+              + "FROM customlink l, element e, unit u "
+              + "WHERE type IN :types AND domain_id = :domainId "
+              + "AND source_id = e.db_id "
+              + "AND e.owner_id = u.db_id "
+              + "AND e.owner_id = :unitId "
+              + "AND u.client_id = :clientId ",
+      nativeQuery = true)
+  Set<FlyweightLink> findAllLinks(
+      @Param("types") Set<String> types,
+      @Param("domainId") String domainId,
+      @Param("unitId") String unitId,
+      @Param("clientId") String clientId);
+
+  default Set<FlyweightElement> findAllLinksGroupedByElement(
+      Set<String> types, String domainId, String unitId, String clientId) {
+    Set<FlyweightLink> allFlyweightElements = findAllLinks(types, domainId, unitId, clientId);
+    Map<String, FlyweightElement> elementsById =
+        allFlyweightElements.stream()
+            .collect(Collectors.groupingBy(FlyweightLink::getSourceId))
+            .entrySet()
+            .stream()
+            .map(entry -> new FlyweightElementData(entry.getKey(), new HashSet<>(entry.getValue())))
+            .collect(Collectors.toMap(k -> k.sourceId(), Function.identity()));
+
+    Set<FlyweightElement> allLeafs =
+        allFlyweightElements.stream()
+            .collect(Collectors.groupingBy(FlyweightLink::getTargetId))
+            .entrySet()
+            .stream()
+            .map(
+                e ->
+                    elementsById.getOrDefault(
+                        e.getKey(), new FlyweightElementData(e.getKey(), new HashSet<>())))
+            .collect(Collectors.toSet());
+
+    Set<FlyweightElement> allNonLeafs =
+        elementsById.entrySet().stream().map(e -> e.getValue()).collect(Collectors.toSet());
+    Set<FlyweightElement> all = new HashSet<>(allLeafs);
+    all.addAll(allNonLeafs);
+    return all;
+  }
+}
