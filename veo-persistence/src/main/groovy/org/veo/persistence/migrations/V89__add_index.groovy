895e6c978e170c17d9aa9f2cdc2a7822d5fb85c0
Jochen Kemnade <jk@sernet.de>
2024-06-27 13:25:15 +0200

    add another index for ProfileItemDataRepository#findAllByIds
    verinice-veo#2855

---
+++ veo-persistence/src/main/groovy/org/veo/persistence/migrations/V89__add_index.groovy
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * verinice.veo
+ * Copyright (C) 2024  Jochen Kemnade
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.veo.persistence.migrations
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+import groovy.sql.Sql
+
+class V89__add_index extends BaseJavaMigration {
+    @Override
+    void migrate(Context context) throws Exception {
+        new Sql(context.connection).with {
+            execute("""
+                create index IDX_PROFILE_ITEM_SYMBOLIC_DB_ID on profile_item(symbolic_db_id);
+        """.toString())
+        }
+    }
+}
